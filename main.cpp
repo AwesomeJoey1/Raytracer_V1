345bd8f610aafedd835629969524a1dfe389cce4
Joey <klein.winz@web.de>
2020-02-03 17:23:32 +0100

    [BASIC] implement diffuse material.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "float.h"
+#include <glm/glm/gtx/norm.hpp>
 
 #include "HittableList.h"
 #include "Sphere.h"
@@ -7,13 +8,31 @@
 #include "Camera.h"
 #include "Random.h"
 
-// background color that is the linear interpolation depending on the y value of the direction of the ray
-glm::vec3 color(Ray &ray, Hittable *world)
+// Searches a point in the unit sphere, by rejecting all points which squared length of the origin vector exceeds 1.0
+glm::vec3 randomInUnitSphere()
+{
+    glm::vec3 p;
+    do {
+        p = glm::vec3(randomDoubleCPP(), randomDoubleCPP(), randomDoubleCPP());
+        p = 2.0f * p - glm::vec3(1,1,1);
+    } while(glm::length2(p) >= 1.0f);
+    return p;
+}
+
+// Calculates pixel color. Background is a color gradient from skyblue to white.
+glm::vec3 color(const Ray &ray, Hittable *world, int depth)
 {
     hitRecord rec;
     if (world->hit(ray, 0.001f, FLT_MAX, rec))
     {
-        return 0.5f*glm::vec3(rec.n.x + 1, rec.n.y + 1, rec.n.z + 1);
+        if (depth < 5000)
+        {
+        glm::vec3 target = rec.p + rec.n + randomInUnitSphere();
+        return 0.5f*color(Ray(rec.p, target - rec.p), world, depth+1);
+        }
+        else {
+            return glm::vec3(0,0,0);
+        }
     } else
     {
         glm::vec3 unit_direction = glm::normalize(ray.direction());
@@ -25,7 +44,7 @@ glm::vec3 color(Ray &ray, Hittable *world)
 int main() {
     const int pic_x = 2000;
     const int pic_y = 1000;
-    const int pic_s = 100;
+    const int pic_s = 10;
 
     Image image(pic_x, pic_y);
 
@@ -44,9 +63,10 @@ int main() {
                 float v = float(j + randomDoubleC()) / float(pic_y);
 
                 Ray ray = camera.getRay(u,v);
-                col += color(ray, world);
+                col += color(ray, world, 0);
             }
             col /= pic_s;
+            col = glm::sqrt(col); // Gamma 2 correction: (color)^(1/gamma) equals taking the square root with gamma = 2
 
 
             int ir = int(255.99 * col.r);
